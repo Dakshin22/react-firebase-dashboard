3daa260de4af6ec833108af782c889051828a948
Dakshin R <drathan45@gmail.com>
2021-01-31 12:35:00 -0500

    authentication

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
 import React from 'react';
 import Header from './Header';
 import './App.css';
-
+import './firebase/config'
+import './pages/Signup'
+import {Route, Switch, BrowserRouter as Router} from 'react-router-dom'
+import Signup from './pages/Signup';
 function App() {
   return (
+    <Router>
     <div>
       <Header></Header>
       <div className="app">
         <div className="ui grid container"></div>
+        <Switch>
+          <Route exact path='/signup' component = {Signup}/>
+        </Switch>
+
       </div>
     </div>
+    </Router>
   );
 }
 
